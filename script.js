f01871628b6fb9c8cc8d4e0b028cb5d18eea52bc
Pritam <pritam7599@gmail.com>
2024-07-09 16:22:01 +0530

    video play

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const VolumeUp = document.querySelector('#VolumeUp');
 const VolumeDown = document.querySelector('#VolumeDown');
 const videoBtn = document.querySelector('#videoBtn');
 const videoInput = document.querySelector('#videoInput');
-
-const VideoPlayer = document.querySelector('#main')
+const videoPlayer = document.querySelector('#main');
 
 const HandleInput = () =>{
     videoInput.click();
@@ -13,13 +12,28 @@ const HandleInput = () =>{
 }
 
 const acceptInputHandler = (obj) => {
-    const selectVideo = obj.target.files[0];
-    const videoElement = document.createElement('video');
-    videoElement.src = link;
-    videoElement.setAttribute('class','video');
-    videoElement.controls="true";
-    VideoPlayer.appendChild(videoElement);
-    videoElement.play();
+    const selectedVideo = obj.target.files[0];
+// src -> BASE64
+const link = URL.createObjectURL(selectedVideo);
+const videoElem = document.createElement('video');
+videoElem.src = link;
+videoElem.setAttribute('class','video')
+videoElem.muted = true;
+videoElem.autoplay = true;
+// videoElem.loop = true;
+videoElem.controls = true;
+videoPlayer.appendChild(videoElem);
+// videoElem.style.height = "100%";
+videoElem.play();
+
+// inc/dec the volume
+// inc/dec speed of video
+
 }
+
+
+
+//browser call function
 videoBtn.addEventListener('click',HandleInput);
 videoInput.addEventListener('change',acceptInputHandler);
+videoElem.addEventListener('click')
