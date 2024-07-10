8380ea4344bab2ba8b82743f2d68a1d4d70e957c
Pritam <pritam7599@gmail.com>
2024-07-10 14:10:30 +0530

    Volume-Speed Up/Down

--- script.js
+++ script.js
@@ -10,30 +10,46 @@ const HandleInput = () =>{
     videoInput.click();
     
 }
-
+const videoElem = document.createElement('video');
 const acceptInputHandler = (obj) => {
     const selectedVideo = obj.target.files[0];
 // src -> BASE64
 const link = URL.createObjectURL(selectedVideo);
-const videoElem = document.createElement('video');
 videoElem.src = link;
 videoElem.setAttribute('class','video')
-videoElem.muted = true;
-videoElem.autoplay = true;
+// videoElem.muted = true;
+// videoElem.autoplay = true;
 // videoElem.loop = true;
 videoElem.controls = true;
 videoPlayer.appendChild(videoElem);
 // videoElem.style.height = "100%";
-videoElem.play();
-
-// inc/dec the volume
-// inc/dec speed of video
-
+// videoElem.play();
 }
 
 
+// ------inc/dec the volume--------
+const increaseVolume = () => {
+    if (videoElem && videoElem.volume < 1 ){
+        videoElem.volume = Math.min(1,videoElem.volume + 0.1 );
+    }
+}
+const decreaseVolume = () => {
+    if (videoElem && videoElem.volume > 0){
+        videoElem.volume = Math.max(0,videoElem.volume - 0.1 );
+    }
+}
+// ------inc/dec the Speed--------
+const increaseSpeed = () => {
+    videoElem.playbackRate = 5;
+}
+const decreaseSpeed = () => {
+    videoElem.playbackRate = 0.5;
+}
 
 //browser call function
 videoBtn.addEventListener('click',HandleInput);
 videoInput.addEventListener('change',acceptInputHandler);
-videoElem.addEventListener('click')
+VolumeUp.addEventListener('click',increaseVolume);
+VolumeDown.addEventListener('click',decreaseVolume);
+SpeedUp.addEventListener('click',increaseSpeed);
+SpeedDown.addEventListener('click',decreaseSpeed);
