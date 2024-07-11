dd5cfd026a7cc672e96350c9e4949f0e4be7ce6e
Pritam <pritam7599@gmail.com>
2024-07-11 19:51:05 +0530

    volume-speed up/down

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ const videoBtn = document.querySelector('#videoBtn');
 const videoInput = document.querySelector('#videoInput');
 const videoPlayer = document.querySelector('#main');
 
+
+
+
 const HandleInput = () =>{
     videoInput.click();
     
 }
-const videoElem = document.createElement('video');
+
 const acceptInputHandler = (obj) => {
+    const videoElem = document.createElement('video');
     const selectedVideo = obj.target.files[0];
 // src -> BASE64
 const link = URL.createObjectURL(selectedVideo);
@@ -24,32 +28,61 @@ videoElem.controls = true;
 videoPlayer.appendChild(videoElem);
 // videoElem.style.height = "100%";
 // videoElem.play();
-}
-
-
-// ------inc/dec the volume--------
-const increaseVolume = () => {
-    if (videoElem && videoElem.volume < 1 ){
-        videoElem.volume = Math.min(1,videoElem.volume + 0.1 );
-    }
-}
-const decreaseVolume = () => {
-    if (videoElem && videoElem.volume > 0){
-        videoElem.volume = Math.max(0,videoElem.volume - 0.1 );
-    }
+videoElem.volume  = 0.3;
 }
 // ------inc/dec the Speed--------
-const increaseSpeed = () => {
-    videoElem.playbackRate = 5;
+const SpeedUpHandler = () => {
+    const videoElem = document.querySelector('video');
+    if (videoElem == null) {
+        return;
+    }
+    if(videoElem.playbackRate > 3){
+    return;
+    }
+    const increaseSpeed = videoElem.playbackRate + 0.5;
+    videoElem.playbackRate = increaseSpeed;
+    console.log(increaseSpeed)
 }
-const decreaseSpeed = () => {
-    videoElem.playbackRate = 0.5;
+const SpeedDownHandler = () => {
+    const videoElem = document.querySelector('video');
+    if (videoElem == null) {
+        return;
+        }
+        if(videoElem.playbackRate > 0){
+            const decreaseSpeed = videoElem.playbackRate - 0.5;
+            videoElem.playbackRate = decreaseSpeed;
+            console.log(decreaseSpeed)
+            }           
+}
+
+// ------inc/dec the volume--------
+const  VolumeUpHandler = () => {
+    const videoElem = document.querySelector('video');
+    if (videoElem == null) {
+        return;
+        }
+        if (videoElem.volume > 0.99) {
+            return;
+            }
+        videoElem.volume=videoElem.volume + 0.1;
+        console.log(videoElem.volume);
+}
+const  VolumeDownHandler = () =>{
+    const videoElem = document.querySelector('video');
+    if (videoElem == null) {
+        return;
+        }
+        if (videoElem.volume <= 0.1) {
+            return videoElem.volume = 0;
+            }
+            videoElem.volume=videoElem.volume - 0.1;
+            console.log(videoElem.volume);
 }
 
 //browser call function
 videoBtn.addEventListener('click',HandleInput);
 videoInput.addEventListener('change',acceptInputHandler);
-VolumeUp.addEventListener('click',increaseVolume);
-VolumeDown.addEventListener('click',decreaseVolume);
-SpeedUp.addEventListener('click',increaseSpeed);
-SpeedDown.addEventListener('click',decreaseSpeed);
+SpeedUp.addEventListener('click',SpeedUpHandler);
+SpeedDown.addEventListener('click',SpeedDownHandler);
+VolumeUp.addEventListener('click',VolumeUpHandler);
+VolumeDown.addEventListener('click',VolumeDownHandler);
